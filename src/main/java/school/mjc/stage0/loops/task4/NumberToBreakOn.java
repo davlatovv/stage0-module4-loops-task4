cdd22534126ddd2dca1bd79455719057e5e1691b
davlatovv <zafargithub@gmail.com>
2023-03-02 11:00:56 +0500

    Loop keywords

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
-
+        for(int i=1;i<=numberToGoUntil;i++){
+            if (i > toBreakWith){
+                break;
+            }
+            if(toBreakWith > numberToGoUntil){
+                System.out.println("iterating till the end");
+                break;
+            }
+            System.out.println(i);
+        }
     }
 }
